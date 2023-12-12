d7e6d9caf039cf2a1bbd4eca27a9f8e58aa6aedf
jessicanobre <150728052+jessicanobre@users.noreply.github.com>
2023-12-12 15:25:15 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,178 @@
+var produtos = [
+
+    {
+        nome: "Noite Encantada",
+        desc: "Explore a moda atemporal da Noite Encantada- uma regata preta adornada com detalhes em renda,proporcionando uma mistura única de sofisticação e estilo.",
+        valor: 159.99,
+        imagem:"pasta/imagem",
+        categoria:"blusas",
+    },
+    {
+        nome: "Cropped Eclipse",
+        desc: "O Cropped Eclipse é a expressão máxima da moda em preto.Sua ousada corda de amarrar passa frente,criando um visual único,enquanto se entrelaça com estilo atrás,proporcionando um toque de modernidade e charme irresistível.",
+        valor: 139.99,
+        imagem:"pasta/imagem",
+        categoria:"blusas",
+    },
+
+    {
+        nome: "Blusa Aurora",
+        desc: "A Blusa Aurora é uma peça elegante e moderna,com um design único que destaca os ombros,proporcionando um toque de sofisticação e conforto.Feita em tecido leve,é perfeita para criar looks versáteis e estilosos em diversas graças.",
+        valor: 229.99,
+        imagem:"pasta/imagem",
+        categoria:"blusas",
+    },
+    {
+        nome: "Blusa Quadriculada Elegante",
+        desc: "Explore a sofisticação da Blusa Quadriculada Elegante,com suas listras pretas formando quadros delicados.O design único,aberto aos ombros,adiciona um toque moderno à sua aparência,combinado estilo e conforto em uma peça única.",
+        valor: 149.99,
+        imagem:"pasta/imagem",
+        categoria:"blusas",
+    },
+
+    {
+        nome: "Lunar Elegante",
+        desc: "Aprensentado bolinhas brancas encantadoras,uma abertura sutil na perna direita para um toque de ousadia,e um detalhe delicado no peito para realçar sua sofisticação.",
+        valor: 299.99,
+        imagem:"pasta/imagem",
+        categoria:"vestidos",
+    },
+    {
+        nome: "Vestido Alvorada",
+        desc: "O Vestido Alvorada é uma elegante peça branca curta,esculpida para abraçar graciosamente as curvas do corpo,destacando a feminalidade com um toque de simplicidade sofisticado.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"vestidos",
+    },
+    {
+        nome: "Elegância Bordeuax",
+        desc: "Este vestido deslumbrante com sombras escuras é a personificação da moda.Com um decote no peito que adiciona um toque de sensualidade refinada,este vestido é perfeito para graças especias onde o estilo encontra a sofisticação.O tecido de alta qualidade proporciona conforto,enquanto o corte impecável destaca a beleza natural de quem veste.O Elegância Bordeuax é a escolha ideal para quem busca um visual impactante e inesquecível.",
+        valor: 229.99,
+        imagem:"pasta/imagem",
+        categoria:"vestidos",
+    },
+    {
+        nome: "Estrela Noturna",
+        desc: "Este elegante vestido curto apresenta um design sofisticado,com um deslumbrante risco prata que percorre o lado esquerdo,proporcionando um toque de beleza.",
+        valor: 159.99,
+        imagem:"pasta/imagem",
+        categoria:"vestidos",
+    },
+
+    {
+        nome: "Rosinha Encanto",
+        desc: "A rasteirinha rosa Rosinha Encdanto é delicadamente adornada com detalhes em rosa forte,amarelo fraco,rosa bebê,conforto não apenas conforto,mas também um toque de elegância aos seus passos.Seu desing encantador combina estilos e conhecimentos,tornando-a a escolha perfeita para quem busca  charme e leveza em cada passo.",
+        valor: 159.99,
+        imagem:"pasta/imagem",
+        categoria:"calçados",
+    },
+    {
+        nome: "Passo Tigre",
+        desc: "Experimente a selvageria da moda com o Tigre Stride,um tênis arrojado adornado com listras inspiradas no majestoso padrão do tigre.Desenvolvido para proporcinar conforto e estilo,este calçado é a escolha perfeita para quem busca uma mistura única de elegante e ousadia em cada passo.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"calçados",
+    },
+    {
+        nome: "Elegância Dourada",
+        desc: "Apresentamos o Tamanco Elegância Dourada,um calçado sofisticado",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"calçados",
+    },
+    {
+        nome: "Passo Tigre",
+        desc: "Experimente a selvageria da moda com o Tigre Stride,um tênis arrojado adornado com listras inspiradas no majestoso padrão do tigre.Desenvolvido para proporcinar conforto e estilo,este calçado é a escolha perfeita para quem busca uma mistura única de elegante e ousadia em cada passo.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"calçados",
+    },
+
+    {
+        nome: "Ébano Elegante",
+        desc: "O casaco Ébano Elegante é uma peça de luxo que combina sofisticação e conforto.Feito com pele sintética de alta qualidade,este casaco preto é a escolha perfeita para adicionar um toque de glamour a qualquer ocasião.Seu design atemporal e corte impecável garante que você fique envolto em estilo e calor,enquanto a textura suave e macia da pele sintética proporciona uma experiência de uso específica agradável.",
+        valor: 199.99,
+        imagem:"pasta/imagem",
+        categoria:"casacos",
+    },
+    {
+        nome: "Rosé Elegante",
+        desc: "O casaco Rosé Elegante é uma peça de luxo feita com pelos sintéticos macios e aconchegantes.Sua atenção rosa suave adiciona um toque de feminalidade e sofisticação.Com um corte elegante e forro acetinado,este casaco é perfeito para dias frios com estilo e conforto.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"casacos",
+    },
+    {
+        nome: "Elegância Dourada",
+        desc: "O casaco Eterno Alvorada é uma peça de elegância atemporal.Seu luxuoso revestimento de pele branca proporciona um toque suave e caloroso,enquanto o design sofisticado e moderno eleva o estilo a novas alturas.Este casaco não é apenas uma peça de vestuário,é uma declaração de moda que transcende as estações,destacando a beleza radiante em qualquer ambiente.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"casacos",
+    },
+    {
+        nome: "Passo Tigre",
+        desc: "Experimente a selvageria da moda com o Tigre Stride,um tênis arrojado adornado com listras inspiradas no majestoso padrão do tigre.Desenvolvido para proporcinar conforto e estilo,este calçado é a escolha perfeita para quem busca uma mistura única de elegante e ousadia em cada passo.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"casacos",
+    },
+
+
+    {
+        nome: "Obsidiana Chic",
+        desc: "A bolsa Obsidiana Chic é uma peça refinada que eleva imediatamente qualquer visula.Sua tonalidade profunda de preto,combina com detalhes em metal prateado,empresta um toque contemporâneo e elegantes.Feita com materias de alta qualidade,essa bolsa oferece não apenas estilo,mas também funcionalidade,com bolsos internos inteligentemente projetados para atender ás necessidades do dia a dia com graciosidade.",
+        valor: 129.99,
+        imagem:"pasta/imagem",
+        categoria:"bolsas",
+    },
+    {
+        nome: "Primavera Encantada",
+        desc: "Deslumbre-se com nossa bolsa de Primavera encantada,uma fusão perfeita de tons verde pastel e rosa suave.Agradável com uma corrente delicada na frente,esta é a expressão perfeita de moda e estilo,trazendo consigo a leveza a beleza da estação das flores.",
+        valor: 250.99,
+        imagem:"pasta/imagem",
+        categoria:"bolsas",
+    },
+    {
+        nome: "Luxo Alpino",
+        desc: "A bolsa Luxo Alpino combina elegâcia e sofisticação,apresentando um design branco imaculado acentuado por detalhes dourados que evocam um toque de glamour.Com espaços amplos e compartimentos organizados,esta bolsa é o acessório perfeito para quem busca estilo e praticidade em cada detalhes.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"bolsas",
+    },
+    {
+        nome: "Passo Tigre",
+        desc: "Experimente a selvageria da moda com o Tigre Stride,um tênis arrojado adornado com listras inspiradas no majestoso padrão do tigre.Desenvolvido para proporcinar conforto e estilo,este calçado é a escolha perfeita para quem busca uma mistura única de elegante e ousadia em cada passo.",
+        valor: 239.99,
+        imagem:"pasta/imagem",
+        categoria:"bolsas",
+    },
+];
+
+function pesquisar() {
+    var pesquisa = document.getElementById("pesquisa").value;
+  
+    var produtosFiltrados = produtos.filter(produto => {
+      return produto.nome.toLowerCase().includes(pesquisa.toLowerCase()) ||
+        produto.valor.toString().toLowerCase().includes(pesquisa.toLowerCase()) ||
+        produto.categoria.toLowerCase().includes(pesquisa.toLowerCase());
+    });
+    if (!pesquisa) {
+      produtosFiltrados = produtos;
+    }
+    var html = "";
+    produtosFiltrados.forEach(produto => {
+      html += `
+        <div>
+          <img src="${produto.imagem}">
+          <p>${produto.nome}</p>
+          <p>R$ ${produto.valor}</p>
+        </div>
+      `;
+    });
+  
+    document.getElementById("produtos").innerHTML = html;
+  }
+  
+  document.getElementById("pesquisa").addEventListener("input", pesquisar);
+  
+  pesquisar();
